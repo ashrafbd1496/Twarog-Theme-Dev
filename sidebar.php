a61a17e0f5e5bded9aefb0b30164759bd698dfa3
ashrafbd1496 <asraf1496@gmail.com>
2023-01-06 15:31:40 +0000

    menu created

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The sidebar containing the main widget area
  *
@@ -6,15 +7,15 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+if (!is_active_sidebar('sidebar-1')) {
 	return;
 }
 ?>
 
 <div class="col-md-4 widget-area" id="secondary">
 
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<?php dynamic_sidebar('sidebar-1'); ?>
 
 </div><!-- #secondary -->
